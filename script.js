d6e834ba56ca8a4974cefeeaa96f5cc9ed4204b8
osama babiker <u.osamababiker@gmail.com>
2024-04-25 11:19:06 +0400

    adding the moon

--- script.js
+++ script.js
@@ -57,7 +57,6 @@ scene.add(sunLight);
 const canvas = document.querySelector("canvas.webgl")
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
-  antialias: true
 })
 renderer.setSize(sizes.width, sizes.height)
 
@@ -69,75 +68,15 @@ const tick = () => {
   requestAnimationFrame(tick)
 
   earth.rotation.y += 0.01
-  moon.rotation.y += 0.001
+  moon.rotation.y += 0.005
   renderer.render(scene, camera)
 }
 
 tick()
 
-// const earthGroup = new THREE.Group();
-// earthGroup.rotation.z = -23.4 * Math.PI / 180;
-// scene.add(earthGroup);
-// new OrbitControls(camera, renderer.domElement);
-// const detail = 12;
-// const loader = new THREE.TextureLoader();
-// const geometry = new THREE.IcosahedronGeometry(1, detail);
-// const material = new THREE.MeshPhongMaterial({
-//   map: loader.load("./static/00_earthmap1k.jpg"),
-//   specularMap: loader.load("./static/02_earthspec1k.jpg"),
-//   bumpMap: loader.load("./static/01_earthbump1k.jpg"),
-//   bumpScale: 0.04,
-// });
-// const earthMesh = new THREE.Mesh(geometry, material);
-// earthGroup.add(earthMesh);
-
-// const lightsMat = new THREE.MeshBasicMaterial({
-//   map: loader.load("./static/03_earthlights1k.jpg"),
-//   blending: THREE.AdditiveBlending,
-// });
-// const lightsMesh = new THREE.Mesh(geometry, lightsMat);
-// earthGroup.add(lightsMesh);
-
-// const cloudsMat = new THREE.MeshStandardMaterial({
-//   map: loader.load("./static/04_earthcloudmap.jpg"),
-//   transparent: true,
-//   opacity: 0.8,
-//   blending: THREE.AdditiveBlending,
-//   alphaMap: loader.load('./static/05_earthcloudmaptrans.jpg'),
-//   // alphaTest: 0.3,
-// });
-// const cloudsMesh = new THREE.Mesh(geometry, cloudsMat);
-// cloudsMesh.scale.setScalar(1.003);
-// earthGroup.add(cloudsMesh);
-
-// const fresnelMat = getFresnelMat();
-// const glowMesh = new THREE.Mesh(geometry, fresnelMat);
-// glowMesh.scale.setScalar(1.01);
-// earthGroup.add(glowMesh);
-
-// const stars = getStarfield({numStars: 2000});
-// scene.add(stars);
-
-// const sunLight = new THREE.DirectionalLight(0xffffff);
-// sunLight.position.set(-2, 0.5, 1.5);
-// scene.add(sunLight);
-
-// function animate() {
-//   requestAnimationFrame(animate);
-
-//   earthMesh.rotation.y += 0.002;
-//   lightsMesh.rotation.y += 0.002;
-//   cloudsMesh.rotation.y += 0.0023;
-//   glowMesh.rotation.y += 0.002;
-//   stars.rotation.y -= 0.0002;
-//   renderer.render(scene, camera);
-// }
-
-// animate();
-
-// function handleWindowResize () {
-//   camera.aspect = window.innerWidth / window.innerHeight;
-//   camera.updateProjectionMatrix();
-//   renderer.setSize(window.innerWidth, window.innerHeight);
-// }
-// window.addEventListener('resize', handleWindowResize, false);
+function handleWindowResize () {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+window.addEventListener('resize', handleWindowResize, false);
